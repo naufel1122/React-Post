954f0c9df0bf57fd2100710a077c70f82d671120
Muhammad Nofil <nofil.khan037@gmail.com>
2023-08-30 16:15:32 +0500

    post created

--- post/src/index.jsx
+++ post/src/index.jsx
@@ -1,21 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css"
+import "./index.css";
+import MyInmage from "./MyImage.png";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import{HandThumbsUp} from "react-bootstrap-icons";
 
 function Post(props) {
-  return(
-  <div className="post" >
-    <div className="Post-2">
-    <h1> {props.name} </h1>
-    <div>{props.date}</div>
-    <p>{props.text}</p>
+  return (
+    <div className="post">
+      <div className="Post-2">
+        <div className="post-head">
+          <img src={props.MyInmage} width={65} height={65} alt="" />
+          <div className="h1">
+            <h1> {props.name} </h1>
+            <div className="date">{props.date}</div>
+          </div>
+        </div>
+        <p>{props.text}</p>
+        <div className="postFooter">
+          <div className="button">
+            {/* <HandThumbsUp  /> */}
+            <span>like</span>
+          </div>
+          <div className="button">
+            {/* <ArrowRight /> */}
+            <span>Comment</span>
+          </div>
+          <div className="button">
+            {/* <ArrowRight /> */}
+            <span>Share</span>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 ReactDOM.render(
   <Post
-    name="Naufel"
+    MyInmage={MyInmage}
+    name="Muhammad Naufel"
     date="1-Sep-2023 5:44PM"
     text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. "
   />,
