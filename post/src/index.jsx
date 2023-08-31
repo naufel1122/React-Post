5f7037effaa3d0d79ab08419e444d74d1c102660
Muhammad Nofil <nofil.khan037@gmail.com>
2023-08-31 23:44:48 +0500

    Hosting Completed

--- post/src/index.jsx
+++ post/src/index.jsx
@@ -5,6 +5,10 @@ import MyInmage from "./MyImage.png";
 import comment from "./chat-left.svg";
 import like from "./hand-thumbs-up.svg";
 import share from "./share.png";
+import ElonSmoke from "./elon-smoke.jpg";
+import elonProfile from "./Elon.jpg";
+import billgates from "./BillGates.jpg";
+import billpost from "./billpost.jpg";
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import { HandThumbsUp } from "react-bootstrap-icons";
 
@@ -13,14 +17,14 @@ function Post(props) {
     <div className="post">
       <div className="Post-2">
         <div className="post-head">
-          <img src={props.MyInmage} width={65} height={65} alt="" />
+          <img src={props.MyInmage} width={60} height={60} alt="" />
           <div className="h1">
             <h1> {props.name} </h1>
             <div className="date">{props.date}</div>
           </div>
         </div>
         <p>{props.text}</p>
-        <img src={props.MyInmage} alt="" />
+        <img src={props.img}  alt="" />
         <hr className="hr" />
         <div className="postFooter">
           <div className="button">
@@ -57,19 +61,21 @@ ReactDOM.render(
     share={share}
     />
     <Post
-    MyInmage={MyInmage}
-    name="Muhammad Naufel"
+    MyInmage={elonProfile}
+    name="Elon Sutta"
     date="1-Sep-2023 5:44PM"
     text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. "
+    img={ElonSmoke}
     like={like}
     comment={comment}
     share={share}
     />
     <Post
-    MyInmage={MyInmage}
-    name="Muhammad Naufel"
+    MyInmage={billgates}
+    name="Bill Gates"
     date="1-Sep-2023 5:44PM"
     text="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. "
+    img={billpost}
     like={like}
     comment={comment}
     share={share}
